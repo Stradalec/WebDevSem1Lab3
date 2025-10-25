22565077dcdb186bb34d193874eef5b5955df501
Stradalec <pochtatipo6@gmail.com>
2025-10-25 11:51:42 +0700

    refactor: prepare to start work

--- WedDevSem1Lab3/src/components/main.jsx
+++ WedDevSem1Lab3/src/components/main.jsx
@@ -1,5 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './App.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './App.css';
+import App from './App.jsx';
 
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
