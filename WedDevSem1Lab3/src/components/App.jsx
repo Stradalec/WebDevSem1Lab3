32583e8731ed21d40143c4fb4d6979438d4b0660
Stradalec <pochtatipo6@gmail.com>
2025-10-25 12:15:02 +0700

    refactor: clean HTML file

--- WedDevSem1Lab3/src/components/App.jsx
+++ WedDevSem1Lab3/src/components/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from '../assets/pictures/delete.svg'
-import viteLogo from '../assets/pictures/delete.svg'
 import './App.css'
 
 function App() {
@@ -16,7 +14,54 @@ function App() {
   }
 } 
 
-  
+  return (
+    <>
+    <header></header>
+    <main className="main">
+      <section className="input_row">
+        <div className="input_column">
+          <input type="text" placeholder="Название" />
+          <input type="text" placeholder="Описание" />
+        </div>
+        <button id = "add" type="button" value="button" className="button_add">+</button>
+      </section>
+      <section className="no_task_window">
+        <div></div>
+        <p>Нет задач</p>
+        <div></div>
+      </section>
+      <section className="dialog_window">
+        <p>Удалить задачу?</p>
+         <div className="input_row_close">
+          <button id = "delete_confirm" type="button" value="button" className="button_dialog">
+            Да
+          </button>
+          <button id = "delete_cancel" type="button" value="button" className="button_dialog">
+            Нет
+          </button>
+        </div>
+      </section>
+      <section className="share_window">
+        <button type="button" value="button" className="button_share">
+          <img src="assets/vector/copy.svg" alt="Удалить заметку" />
+        </button>
+        <button type="button" value="button" className="button_share">
+          <img src="assets/vector/vk.svg" alt="Поделиться" />
+        </button>
+        <button type="button" value="button" className="button_share">
+          <img src="assets/vector/telegram.svg" alt="Редактировать заметку" />
+        </button>
+        <button type="button" value="button" className="button_share">
+          <img src="assets/vector/whatsapp.svg" alt="Информация о заметке" />
+        </button>
+        <button type="button" value="button" className="button_share">
+          <img src="assets/vector/facebook.svg" alt="Поделиться заметкой" />
+        </button>
+      </section>
+    </main>
+    <footer></footer>
+    </>
+  )
 }
 
 export default App
